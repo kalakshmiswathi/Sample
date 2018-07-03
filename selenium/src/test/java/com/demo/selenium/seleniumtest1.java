c85f7dc04d17ca412275cbe75ee4fea9f43bb0c8
Midhuva <Training_B7C.06.13@BDC7-D-DLRGH92.dir.svc.accenture.com>
2018-07-03 12:20:52 +0530

    commit all files

---
+++ selenium/src/test/java/com/demo/selenium/seleniumtest1.java
@@ -0,0 +1,27 @@
+package com.demo.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class seleniumtest1 {
+	@Test
+	public void testgithudlogin()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\training_b7c.06.13\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://github.com/login");
+		driver.findElement(By.id( "login_field")).sendKeys("kalakshmiswathi");
+		driver.findElement(By.id("password")).sendKeys("Swathi@123");
+		driver.findElement(By.name("commit")).click();
+		
+	    String title= driver.getTitle();
+	    Assert.assertEquals(title, "GitHub");
+	    System.out.println("continue with test code");
+	}
+
+}
---
+++ selenium/src/test/java/com/demo/selenium/test1.java
@@ -0,0 +1,16 @@
+package com.demo.selenium;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void testdemowebshop()
+	{
+		System.out.println("in webshop");
+	}
+	@Test
+	public void testpayment()
+	{
+		System.out.println("in testpayment");
+	}
+}
